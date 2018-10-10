36baa82e9fcacbd0b1cc10de7a001b301e454df1
Shivani Agrawal <agrawal.shivanifxbytes.com>
2018-10-10 14:01:29 +0530

    month list views by loop

--- resources/views/admin/monthlyExpenses.blade.php
+++ resources/views/admin/monthlyExpenses.blade.php
@@ -2,7 +2,7 @@
 @section('content')       
 <div class="row">
     <div class="col-lg-12">
-        <h3 class="page-header"><i class="fa fa-table"></i>{{ __('messages.maintenance_transaction') }}<a class="btn btn-primary pull-right" href=" {{ url('/') }}/addMonthlyExpense"> {{__('messages.add')}} </a>
+        <h3 class="page-header"><i class="fa fa-table"></i>{{ __('messages.maintenance_transaction') }}<a class="btn btn-primary pull-right" href=" {{ url('/') }}/addMaintenanceTransaction"> {{__('messages.add')}} </a>
         </h3>
         <ol class="breadcrumb">
             <li><i class="fa fa-home"></i><a href="{{ url('/') }}/dashboard">{{ __('messages.home') }}</a></li>
@@ -12,46 +12,25 @@
 </div>
 <div class="row">
     <div class="col-lg-6 col-md-offset-3">
-<select class="form-control"  id="sel1" name="sellist1">
-    <option>Select Year</option>
-    <option>2018</option>
-    <option>2019</option>
-    <option>2020</option>
-    <option>2021</option>
-    <option>2022</option>
-    <option>2023</option>
-    <option>2024</option>
-    <option>2025</option>
-    <option>2026</option>
-    <option>2027</option>
-    <option>2028</option>
-    <option>2029</option>
-    <option>2030</option>
-    <option>2031</option>
-    <option>2032</option>
-    <option>2033</option>
-    <option>2034</option>
-    <option>2035</option>
-    <option>2036</option>
-    <option>2037</option>
-    <option>2038</option>
-    <option>2039</option>
-</select>
-<hr />
-
+        <?php $current_month = date("m"); 
+              $current_year = date('Y')?>
+        <select class="form-control"  id="sel1" name="sellist1">
+            <option>Select Year</option>
+            @for($i=$current_year; $i>=$current_year-20; $i--)
+            <option>{{ $i }}</option>
+            @endfor
+        </select>
+        <hr />
         <ul class="list-group">
-            <a href="{{ url('/') }}/showMaintenanceTransactionList"><li class="list-group-item">January</li></a>
-            <a href=""><li class="list-group-item">February</li></a>
-            <a href=""><li class="list-group-item">March</li></a>
-            <a href=""><li class="list-group-item">April</li></a>
-            <a href=""><li class="list-group-item">May</li></a>
-            <a href=""><li class="list-group-item">June</li></a>
-            <a href=""><li class="list-group-item">July</li></a>
-            <a href=""><li class="list-group-item">August</li></a>
-            <a href=""><li class="list-group-item">September</li></a>
-            <a href=""><li class="list-group-item">October</li></a>
-            <a href=""><li class="list-group-item">November</li></a>
-            <a href=""><li class="list-group-item">December</li></a>
+            @for($i=1; $i<=$current_month; $i++)
+            <?php  
+            $dateObj   = DateTime::createFromFormat('!m', $i);
+            $monthName = $dateObj->format('F'); 
+            $monthCode = $dateObj->format('m');// March ?>
+            <li  class="list-group-item">
+                <a href="{{$monthCode}}">{{ $monthName }}</a>
+            </li>
+            @endfor
         </ul> 
     </div>
 </div>
