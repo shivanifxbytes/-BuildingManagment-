80fd8e6e3f5e91e7988ab688cce3b6e0071f1180
shriya01 <jain.shriya@fxbytes.com>
2018-10-15 16:19:43 +0530

    unmerged changes

--- resources/views/admin/addMonthlyExpenses.blade.php
+++ resources/views/admin/addMonthlyExpenses.blade.php
@@ -113,16 +113,11 @@
 </div>
 @endsection
 @section('scripts')
-<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.5.0/js/bootstrap-datepicker.js"></script>  
-<script type="text/javascript"> 
-  $(document).ready(function(){
-    $('.date').datepicker({  
-      format: 'mm/dd/yyyy',
-    });  
-  })        
-</script>  
 <script type="text/javascript">
-  $(document).ready(function(){      
+  $(document).ready(function(){
+  $('.date').datepicker({  
+      format: 'mm/dd/yyyy',
+    });        
     var postURL = "<?php echo url('addmore'); ?>";
     var i=1;  
     $('.dynamic-added').remove();
